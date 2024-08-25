76e75f3d5d87cc997b6275661d537ee49f2ebe73
Chukwuebuka98 <emmanuelzico17@gmail.com>
2024-08-25 17:39:15 +0100

    Login form UI updated

--- src/pages/auth/AdminLogin.jsx
+++ src/pages/auth/AdminLogin.jsx
@@ -1,44 +1,97 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 import { useNavigate } from "react-router-dom";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
 
 const AdminLogin = () => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const [authError, setAuthError] = useState(null); // Track authentication errors
   const navigate = useNavigate();
   const auth = getAuth();
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    signInWithEmailAndPassword(auth, email, password)
+  const validationSchema = Yup.object().shape({
+    email: Yup.string()
+      .email("Invalid email address")
+      .required("Email is required"),
+    password: Yup.string()
+      .min(8, "Password must be at least 8 characters")
+      .matches(/^[A-Za-z\d]+$/, "Password can only contain letters and numbers")
+      .required("Password is required"),
+  });
+
+  const handleSubmit = (values, { setSubmitting }) => {
+    setAuthError(null); // Reset error state before attempting login
+    signInWithEmailAndPassword(auth, values.email, values.password)
       .then((userCredential) => {
         navigate("/admin");
       })
       .catch((error) => {
-        console.error("Error logging in", error);
+        setAuthError("Username or password is incorrect"); // Set error message
+        setSubmitting(false);
       });
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <input
-        type="email"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-        placeholder="Email"
-        required
-        className=" p-3 bg-[#0B0B0C] rounded-md max-h-20px text-sm border border-gray-700 shadow-sm focus:outline-none focus:ring-2 focus:ring-customPurple-purple focus:border-customPurple-purple hover:border-customPurple-purple duration-300 "
-      />
-      <input
-        type="password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-        placeholder="Password"
-        required
-        className=" p-3 bg-[#0B0B0C] rounded-md max-h-20px text-sm border border-gray-700 shadow-sm focus:outline-none focus:ring-2 focus:ring-customPurple-purple focus:border-customPurple-purple hover:border-customPurple-purple duration-300 "
-      />
-      <button type="submit">Login</button>
-    </form>
+    <div className="w-full max-w-[1240px] mx-auto flex items-center justify-center h-full my-10 bg-customBlack-dark">
+      <div className="w-[400px] h-[400px] flex flex-col items-center justify-center">
+        <Formik
+          initialValues={{ email: "", password: "" }}
+          validationSchema={validationSchema}
+          onSubmit={handleSubmit}
+        >
+          {({ isSubmitting, errors, touched }) => (
+            <Form className="w-full flex flex-col gap-3">
+              <h3 className="text-2xl">Admin Login</h3>
+              {authError && (
+                <div className="text-red-500 text-sm mb-4">{authError}</div>
+              )}{" "}
+              {/* Display error */}
+              <div>
+                <Field
+                  type="email"
+                  name="email"
+                  placeholder="Email"
+                  className={`p-3 bg-[#0B0B0C] rounded-md text-sm border shadow-sm focus:outline-none focus:ring-2 focus:ring-customPurple-purple focus:border-customPurple-purple hover:border-customPurple-purple duration-300 w-full ${
+                    errors.email && touched.email
+                      ? "border-red-500"
+                      : "border-gray-700"
+                  }`}
+                />
+                <ErrorMessage
+                  name="email"
+                  component="div"
+                  className="text-red-500 text-sm"
+                />
+              </div>
+              <div>
+                <Field
+                  type="password"
+                  name="password"
+                  placeholder="Password"
+                  className={`w-full p-3 bg-[#0B0B0C] rounded-md text-sm border shadow-sm focus:outline-none focus:ring-2 focus:ring-customPurple-purple focus:border-customPurple-purple hover:border-customPurple-purple duration-300 ${
+                    errors.password && touched.password
+                      ? "border-red-500"
+                      : "border-gray-700"
+                  }`}
+                />
+                <ErrorMessage
+                  name="password"
+                  component="div"
+                  className="text-red-500 text-sm"
+                />
+              </div>
+              <button
+                type="submit"
+                disabled={isSubmitting}
+                className="p-3 bg-customPurple-purple hover:bg-customPurple-dark text-white rounded-md duration-300"
+              >
+                {isSubmitting ? "Logging in..." : "Login"}
+              </button>
+            </Form>
+          )}
+        </Formik>
+      </div>
+    </div>
   );
 };
 
--- src/pages/auth/Login.jsx
+++ src/pages/auth/Login.jsx
@@ -2,47 +2,76 @@ import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { getDoc, doc } from "firebase/firestore";
 import { database } from "../../firebaseConfig";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
 
 const Login = () => {
-  const [userID, setUserID] = useState("");
   const [error, setError] = useState("");
   const navigate = useNavigate();
 
-  const handleLogin = async (e) => {
-    e.preventDefault();
+  // Validation schema using Yup
+  const validationSchema = Yup.object().shape({
+    userID: Yup.string().required("User ID is required"),
+  });
+
+  const handleLogin = async (values, { setSubmitting }) => {
+    setError(""); // Reset error state before attempting login
     try {
-      const payScaleDoc = await getDoc(doc(database, "payscales", userID));
+      const payScaleDoc = await getDoc(
+        doc(database, "payscales", values.userID)
+      );
       if (payScaleDoc.exists()) {
-        // Store payScale data
         const payScale = payScaleDoc.data();
         sessionStorage.setItem("payscales", JSON.stringify(payScale));
-
-        // Redirect to home route
-        navigate("/");
+        navigate("/"); // Redirect to home route
       } else {
         setError("Invalid ID. Pay scale not found.");
       }
     } catch (err) {
       console.error("Error fetching pay scale:", err);
       setError("Error logging in. Please try again.");
+    } finally {
+      setSubmitting(false); // Reset Formik's submitting state
     }
   };
 
   return (
-    <div className="w-full max-w-[1240px] m-auto h-screen flex justify-center items-center">
-      <div className="p-10">
-        <h1>Login</h1>
-        <form onSubmit={handleLogin}>
-          <input
-            type="text"
-            value={userID}
-            onChange={(e) => setUserID(e.target.value)}
-            placeholder="Enter User ID"
-            className=" p-3 bg-[#0B0B0C] rounded-md max-h-20px text-sm border border-gray-700 shadow-sm focus:outline-none focus:ring-2 focus:ring-customPurple-purple focus:border-customPurple-purple hover:border-customPurple-purple duration-300 "
-          />
-          <button type="submit">Login</button>
-        </form>
-        {error && <p style={{ color: "red" }}>{error}</p>}
+    <div className="w-full max-w-[1240px] m-auto h-screen flex justify-center items-center bg-customBlack-dark my-10">
+      <div className="w-[400px] h-[400px] flex flex-col items-center justify-center">
+        <Formik
+          initialValues={{ userID: "" }}
+          validationSchema={validationSchema}
+          onSubmit={handleLogin}
+        >
+          {({ isSubmitting }) => (
+            <Form className="w-full flex flex-col gap-3">
+              <h1 className="text-2xl">Login</h1>
+              {error && <p className="text-red-500 text-sm">{error}</p>}
+              <div>
+                <Field
+                  type="text"
+                  name="userID"
+                  placeholder="Enter User ID"
+                  className={`w-full p-3 bg-[#0B0B0C] rounded-md max-h-20px text-sm border shadow-sm focus:outline-none focus:ring-2 focus:ring-customPurple-purple focus:border-customPurple-purple hover:border-customPurple-purple duration-300 ${
+                    error ? "border-red-500" : "border-gray-700"
+                  }`}
+                />
+                <ErrorMessage
+                  name="userID"
+                  component="div"
+                  className="text-red-500 text-sm"
+                />
+              </div>
+              <button
+                type="submit"
+                disabled={isSubmitting}
+                className="mt-4 p-3 bg-customPurple-purple hover:bg-customPurple-dark text-white rounded-md duration-300"
+              >
+                {isSubmitting ? "Logging in..." : "Login"}
+              </button>
+            </Form>
+          )}
+        </Formik>
       </div>
     </div>
   );
